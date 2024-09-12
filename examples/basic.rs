66b98ac2b8e30169b1bd97709a944682b6da9698
David James <davidcjames@gmail.com>
2024-09-12 08:42:59 -0400

    first commit

---
+++ examples/basic.rs
@@ -0,0 +1,22 @@
+use file_region::FileRegion;
+use std::io::{Read, Seek, SeekFrom, Write};
+use tempfile::tempfile;
+
+fn main() -> std::io::Result<()> {
+    let mut file = tempfile()?;
+    file.write_all(b"Hello, FileRegion.")?;
+
+    let mut region = FileRegion::new(&file, 7..16);
+    let mut buffer = [0; 9];
+    region.read(0, &mut buffer)?;
+    assert_eq!(&buffer, b"FileRegio");
+
+    region.write(0, b"01234")?;
+
+    let mut content = String::new();
+    file.seek(SeekFrom::Start(0))?;
+    file.read_to_string(&mut content)?;
+    assert_eq!(content, "Hello, 01234egion.");
+
+    Ok(())
+}
